e61d34a6fd6229b985e59cda10b3060f9dcebeb9
josduaper <josduaper@alu.edu.gva.es>
2024-11-08 12:50:40 +0100

    Cadenas

---
+++ src/main/Parentesis.java
@@ -0,0 +1,33 @@
+public class Parentesis {
+
+    public static boolean esParentizada(String frase) {
+        int parAbierto = 0;
+        int parCerrado = 0;
+
+        for (int i = 0; i < frase.length(); i++) {
+            if (frase.charAt(i) == '(') {
+                parAbierto++;
+            } else if (frase.charAt(i) == ')') {
+                parCerrado++;
+            }
+        }
+
+        if (parAbierto == parCerrado) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        String frase = "Real Académia Española";
+
+        if (esParentizada(frase)) {
+            System.out.println("Está bien parentizada");
+        } else {
+            System.out.println("No está bien parentizada");
+        }
+
+    }
+}
